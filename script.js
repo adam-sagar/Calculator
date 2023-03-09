f405f606536371a9e0975e139e9b5505e412c437
adam-sagar <adamschrissagar@gmail.com>
2023-03-10 10:14:34 +1300

    Changed font and made 2 digit

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-addNumber() {
+let operand1 = '';
+let operand2 = '';
+let operator = '';
 
+function addNumber(number) {
+    // if (operand1 == '') {
+    //     operand1 = number
+    // } else {
+    // operand2 = number}
+    // document.getElementById('results').value = number
+    if (operator == '') {
+        operand1 = operand1.toString() + number.toString();
+        document.getElementById('results').value = operand1
+    } else {
+        operand2 = operand2.toString() + number.toString();
+        document.getElementById('results').value = operand2
+    }
 }
 
-selectOperator() {
-
+function selectOperator(op) {
+    operator = op
 }
 
-calculate() {
-    
+function calculate() {
+    let result;
+    switch (operator) {
+        case '+':
+            result = operand1 + operand2;
+            break;
+        case '-':
+            result = operand1 - operand2;
+            break;
+        case '*':
+            result = operand1 * operand2;
+            break;
+        case '/':
+            result = operand1 / operand2;
+            break;
+    }
+    document.getElementById('results').value = result
 }
 
-clear() {
-
+function clearResult() {
+    document.getElementById('results').value = ''
+    operand1 = ''
+    operand2 = ''
+    operator = ''
 }
